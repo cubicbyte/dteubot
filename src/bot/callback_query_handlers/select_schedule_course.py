a00f5fbaefc0e5d458e10932a7086e0314dd05f2
Angron42 <bmaruhnenko@gmail.com>
2023-01-04 12:01:47 +0200

    Fix the debug line that breaks group selection

--- src/bot/callback_query_handlers/select_schedule_course.py
+++ src/bot/callback_query_handlers/select_schedule_course.py
@@ -10,6 +10,6 @@ def handler(call: telebot.types.CallbackQuery):
     logger.debug('Handling callback query')
     structureId = int(call.args['structureId'])
     facultyId = int(call.args['facultyId'])
-    course = int(call.args['course']) + 9
+    course = int(call.args['course'])
     chat_configs.set_chat_config(call.message.chat.id, call.message.config)
     bot.edit_message_text(**create_select_group_message(call.message, structureId, facultyId, course), message_id=call.message.id)
