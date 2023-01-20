7150868b848cd52df6b0f274363815de9bc9a929
Angron42 <bmaruhnenko@gmail.com>
2023-01-20 13:36:45 +0200

    Finished work on CacheReader

--- lib/cache_reader/__init__.py
+++ lib/cache_reader/__init__.py
@@ -1,28 +1,50 @@
 import sqlite3
-from datetime import date
+from datetime import date as _date
 
 class CacheReader:
     def __init__(self, file: str) -> None:
         self.__connection = sqlite3.connect(file)
 
-    def __example(self):
-        con = self.__connection
-        cur = con.cursor()
-        cur.execute('SELECT * FROM Schedule LIMIT 1')
+    def get_schedule(self, groupId: int, date: str | _date) -> list | None:
+        if type(date) == _date:
+            date = date.strftime('%Y-%m-%d')
+        cur = self.__connection.cursor()
+        cur.execute('SELECT * FROM Schedule WHERE groupId = ? AND date = ?;', (groupId, date))
         res = cur.fetchone()
-        print(res)
+        cur.close()
+        return res
 
-    def get_schedule(self, groupId: int, date: str | date) -> list[dict] | None:
-        pass
+    def get_structures(self) -> list[dict]:
+        cur = self.__connection.cursor()
+        cur.execute('SELECT * FROM Structures;')
+        res = cur.fetchall()
+        cur.close()
+        return res
 
-    def get_structures(self) -> list[dict] | None:
-        pass
+    def get_faculties(self, structureId: int) -> list:
+        cur = self.__connection.cursor()
+        cur.execute('SELECT * FROM Faculties WHERE structureId = ?;', (structureId,))
+        res = cur.fetchall()
+        cur.close()
+        return res
 
-    def get_faculties(self, structureId: int) -> list[dict] | None:
-        pass
+    def get_courses(self, facultyId: int) -> list:
+        cur = self.__connection.cursor()
+        cur.execute('SELECT * FROM Courses WHERE facultyId = ?;', (facultyId,))
+        res = cur.fetchall()
+        cur.close()
+        return res
 
-    def get_courses(self, facultyId: int) -> list[dict] | None:
-        pass
+    def get_groups(self, facultyId: int, course: int) -> list:
+        cur = self.__connection.cursor()
+        cur.execute('SELECT * FROM Groups WHERE facultyId = ? AND course = ?;', (facultyId, course))
+        res = cur.fetchall()
+        cur.close()
+        return res
 
-    def get_groups(self, facultyId: int, course: int) -> list[dict] | None:
-        pass
+    def get_group(self, groupId: int) -> list | None:
+        cur = self.__connection.cursor()
+        cur.execute('SELECT * FROM Groups WHERE id = ?;', (groupId,))
+        res = cur.fetchone()
+        cur.close()
+        return res
