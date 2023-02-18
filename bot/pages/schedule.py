5df43c022756e9b4e7fd1727f892eb9f41b5e87c
Bohdan Marukhnenko <bmaruhnenko@gmail.com>
2023-02-18 17:39:04 +0200

    Reworked mechanism of skipping empty days
    Instead of just skipping days without lessons (which was confusing to users before), now they see a menu that says the number of days without lessons.
    
    Also refactored bot/pages/schedule.py file

--- bot/pages/schedule.py
+++ bot/pages/schedule.py
@@ -10,32 +10,71 @@ from .api_unavaliable import create_message as create_api_unavaliable_message
 
 logger = logging.getLogger()
 
-def create_lessons_empty_text(message: types.Message) -> str:
-    line = '—————————————————————————'
-    lessons_missing_text = message.lang['text.subjects.missing']
-    spaces_count = int(len(line) / 2) - int(len(lessons_missing_text) / 2)
 
-    # All this code is needed to center the text
-    if spaces_count < 0:
-        spaces_count = 0
-    elif len(line) % 2 == 0 and len(lessons_missing_text) % 2 != 0:
-        spaces_count -= 1
 
-    spaces = ' ' * spaces_count
-    text = f"`{line}`\n\n`{spaces}``{lessons_missing_text}`\n\n`{line}`"
+def count_no_lesson_days(schedule: list[dict[str, any]], date: _date, direction_right = True) -> timedelta | None:
+    'Counts the number of days without lessons'
+    if not direction_right:
+        schedule = reversed(schedule)
 
-    return text
+    res = None
+    for day in schedule:
+        day_date = datetime.strptime(day['date'], '%Y-%m-%d').date()
+        if direction_right:
+            if day_date > date:
+                res = day_date - date
+                break
+        else:
+            if day_date < date:
+                res = date - day_date
+                break
+
+    return res
+
+def get_localized_date(date: _date, message: types.Message) -> str:
+    date_localized = format_date(date, locale=message.lang_code)
+    week_day_localized = message.lang['text.time.week_day.' + str(date.weekday())]
+    full_date_localized = f"*{date_localized}* `[`*{week_day_localized}*`]`"
+    return full_date_localized
+
+def create_schedule_section(schedule_day: dict[str, any], message: types.Message) -> str:
+    schedule_section = ''
+    for lesson in schedule_day['lessons']:
+        for period in lesson['periods']:
+            # Escape apostrophes in teacher names because they break Telegram Markdown
+            period['teachersName'] = period['teachersName'].replace('`', '\\`')
+            period['teachersNameFull'] = period['teachersNameFull'].replace('`', '\\`')
+
+            # If there are multiple teachers, display the first one and add +1 to the end
+            if ',' in period['teachersName']:
+                count = str(period['teachersNameFull'].count(','))
+                period['teachersName'] = period['teachersName'][:period['teachersName'].index(',')] + ' +' + count
+                period['teachersNameFull'] = period['teachersNameFull'][:period['teachersNameFull'].index(',')] + ' +' + count
+
+            schedule_section += message.lang['text.schedule.period'].format(
+                **period,
+                lessonNumber=lesson['number']
+            )
+
+    schedule_section += '`—――—―``―——``―—―``――``—``―``—``――――``――``―――`'
+    return schedule_section
+
+
 
 def create_message(message: types.Message, date: _date | str) -> dict:
+    # Create "date_str" and "date" variables
     if isinstance(date, _date):
         date_str = date.strftime('%Y-%m-%d')
     else:
         date_str = date
         date = datetime.strptime(date, '%Y-%m-%d').date()
 
-    try:
-        schedule = api.timetable_group(message.config['groupId'], date)
 
+    # Get schedule
+    dateStart = date - timedelta(days=date.weekday() + 7)
+    dateEnd = dateStart + timedelta(days=20)
+    try:
+        schedule = api.timetable_group(message.config['groupId'], dateStart, dateEnd)
     except requests.exceptions.HTTPError as err:
         if err.response.status_code == 422:
             return create_invalid_group_message(message)
@@ -46,71 +85,65 @@ def create_message(message: types.Message, date: _date | str) -> dict:
     ):
         return create_api_unavaliable_message(message)
 
-    page_content = ''
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    current_date = _date.today()
 
-    day_next = None
+    # Find schedule of current day
+    cur_day_schedule = None
     for day in schedule:
-        if day['date'] != date_str:
-            day_next = day['date']
+        if day['date'] == date_str:
+            cur_day_schedule = day
             break
-    if day_next is None:
-        day_next = (date + timedelta(days=1)).strftime('%Y-%m-%d')
 
+
+    # Create the schedule page content
+    if cur_day_schedule is not None:
+        msg_text = message.lang['page.schedule'].format(
+            date=get_localized_date(date, message),
+            schedule=create_schedule_section(cur_day_schedule, message)
+        )
+    # If there is no lesson for the current day
+    else:
+        # Variables with the number of days you need to skip to reach a day with lessons
+        skip_left = count_no_lesson_days(schedule, date, direction_right=False)
+        skip_right = count_no_lesson_days(schedule, date, direction_right=True)
+        if skip_right is None:
+            skip_right = dateEnd - date + timedelta(days=1)
+        if skip_left is None:
+            skip_left = date - dateStart + timedelta(days=1)
+        # If there are no lessons for multiple days
+        # Then combine all the days without lessons into one page
+        if skip_left.days > 1 or skip_right.days > 1:
+            dateStart_localized = get_localized_date(date - skip_left + timedelta(days=1), message)
+            dateEnd_localized = get_localized_date(date + skip_right - timedelta(days=1), message)
+            msg_text = message.lang['page.schedule.empty.multiple_days'].format(
+                dateStart=dateStart_localized,
+                dateEnd=dateEnd_localized
+            )
+        # If no lessons for only one day
+        else:
+            msg_text = message.lang['page.schedule.empty'].format(date=get_localized_date(date, message))
+
+
+
+    # Create buttons
+    if cur_day_schedule is not None:
+        day_next = date + timedelta(days=1)
+        day_prev = date - timedelta(days=1)
+    else:
+        day_next = date + skip_right
+        day_prev = date - skip_left
+    markup = types.InlineKeyboardMarkup(row_width=2)
     buttons = [
-        types.InlineKeyboardButton(text=message.lang['button.navigation.day_previous'], callback_data='open.schedule.day#date=' + (date - timedelta(days=1)).strftime('%Y-%m-%d')),
-        types.InlineKeyboardButton(text=message.lang['button.navigation.day_next'], callback_data='open.schedule.day#date=' + day_next),
+        types.InlineKeyboardButton(text=message.lang['button.navigation.day_previous'], callback_data='open.schedule.day#date=' + day_prev.strftime('%Y-%m-%d')),
+        types.InlineKeyboardButton(text=message.lang['button.navigation.day_next'], callback_data='open.schedule.day#date=' + day_next.strftime('%Y-%m-%d')),
         types.InlineKeyboardButton(text=message.lang['button.navigation.week_previous'], callback_data='open.schedule.day#date=' + (date - timedelta(days=7)).strftime('%Y-%m-%d')),
         types.InlineKeyboardButton(text=message.lang['button.navigation.week_next'], callback_data='open.schedule.day#date=' + (date + timedelta(days=7)).strftime('%Y-%m-%d')),
         types.InlineKeyboardButton(text=message.lang['button.menu'], callback_data='open.menu')
     ]
-
-    if date != current_date:
-        # If the selected day is not today, then add "today" button
-        buttons.insert(
-            -1, types.InlineKeyboardButton(text=message.lang['button.navigation.today'], callback_data='open.schedule.today')
-        )
-
+    # If the selected day is not today, then add "today" button
+    if date != _date.today():
+        buttons.append(types.InlineKeyboardButton(text=message.lang['button.navigation.today'], callback_data='open.schedule.today'))
     markup.add(*buttons)
 
-    # Find day in schedule
-    day_i = None
-    for i in range(len(schedule)):
-        if schedule[i]['date'] == date_str:
-            day_i = i
-            break
-    if day_i is None:
-        page_content = create_lessons_empty_text(message)
-
-    else:
-        # Create page content
-        for lesson in schedule[i]['lessons']:
-            for period in lesson['periods']:
-                period['teachersName'] = period['teachersName'].replace('`', '\'')
-                period['teachersNameFull'] = period['teachersNameFull'].replace('`', '\'')
-
-                # If there are multiple teachers, display the first one and add +1 to the end
-                if ',' in period['teachersName']:
-                    count = str(period['teachersNameFull'].count(','))
-                    period['teachersName'] = period['teachersName'][:period['teachersName'].index(',')] + ' +' + count
-                    period['teachersNameFull'] = period['teachersNameFull'][:period['teachersNameFull'].index(',')] + ' +' + count
-
-                page_content += f"`———— ``{period['timeStart']}`` ——— ``{period['timeEnd']}`` ————`\n"
-                page_content += f"`  `*{period['disciplineShortName']}*`[{period['typeStr']}]`\n"
-                page_content += f"`{lesson['number']} `{period['classroom']}\n`  `{period['teachersNameFull']}\n"
-
-        page_content += '`—――—―``―——``―—―``――``—``―``—``――――``――``―――`'
-
-
-    date_locale = format_date(date, locale=message.lang_code)
-    week_day_locale = message.lang['text.time.week_day.' + str(date.weekday())]
-    date_locale_text = f"*{date_locale}* `[`*{week_day_locale}*`]`"
-
-    msg_text = message.lang['page.schedule'].format(
-        date=date_locale_text,
-        schedule=page_content
-    )
 
     msg = {
         'chat_id': message.chat.id,
