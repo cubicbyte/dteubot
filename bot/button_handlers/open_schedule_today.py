71d33125e31720782f449a9461a759ba729a0379
cubicbyte <bmaruhnenko@gmail.com>
2023-02-25 21:46:08 +0200

    Fixed incorrect arguments

--- bot/button_handlers/open_schedule_today.py
+++ bot/button_handlers/open_schedule_today.py
@@ -1,6 +1,6 @@
 import telebot.types
 import logging
-from datetime import datetime
+from datetime import date
 from ..settings import bot
 from ..pages import schedule
 
@@ -9,4 +9,4 @@ logger = logging.getLogger(__name__)
 @bot.callback_query_handler(func=lambda call: call.query == 'open.schedule.today')
 def handler(call: telebot.types.CallbackQuery):
     logger.debug('Handling callback query')
-    bot.edit_message_text(**schedule.create_message(call.message.lang_code, call.message.config['groupId'], datetime.today()), chat_id=call.message.chat.id, message_id=call.message.message_id)
+    bot.edit_message_text(**schedule.create_message(call.message.lang_code, call.message.config['groupId'], date.today()), chat_id=call.message.chat.id, message_id=call.message.message_id)
--- bot/button_handlers/open_schedule_tomorrow.py
+++ bot/button_handlers/open_schedule_tomorrow.py
@@ -1,6 +1,6 @@
 import telebot.types
 import logging
-from datetime import datetime, timedelta
+from datetime import date as _date, timedelta
 from ..settings import bot
 from ..pages import schedule
 
@@ -9,5 +9,5 @@ logger = logging.getLogger(__name__)
 @bot.callback_query_handler(func=lambda call: call.query == 'open.schedule.tomorrow')
 def handler(call: telebot.types.CallbackQuery):
     logger.debug('Handling callback query')
-    date = datetime.today() + timedelta(days=1)
+    date = _date.today() + timedelta(days=1)
     bot.edit_message_text(**schedule.create_message(call.message.lang_code, call.message.config['groupId'], date), chat_id=call.message.chat.id, message_id=call.message.message_id)
