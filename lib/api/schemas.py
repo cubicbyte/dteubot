cac128ec156fc394d8cfbc2fbc00e78c46f82028
Bohdan Marukhnenko <bmaruhnenko@gmail.com>
2023-03-19 18:07:51 +0200

    Added tooltips to api schemas

--- lib/api/schemas.py
+++ lib/api/schemas.py
@@ -11,104 +11,173 @@ class JSONDeserializable:
 
 @dataclass
 class CallSchedule(JSONDeserializable):
-    timeStart:  str
-    timeEnd:    str
-    number:     int
-    length:     int
+    "Моделька для пар"
+    timeStart: str
+    "Время начала"
+    timeEnd: str
+    "Время конца"
+    number: int
+    "Номер"
+    length: int
+    "Длительность"
 
 @dataclass
 class Chair(JSONDeserializable):
-    id:         int
-    shortName:  str
-    fullName:   str
+    "Моделька для кафедр"
+    id: int
+    "id кафедры"
+    shortName: str
+    "Аббревиатура кафедры"
+    fullName: str
+    "Название кафедры"
 
 @dataclass
 class Classroom(JSONDeserializable):
-    id:         int
-    name:       str
+    "Моделька для учебных аудиторий"
+    id: int
+    "id аудитории"
+    name: str
+    "Название аудитории"
     countPlace: int
-    type:       int
+    "Количество мест"
+    type: int
+    "Тип 0-учебная 1-лаборатория"
 
 @dataclass
 class Course(JSONDeserializable):
+    "Моделька для курсов"
     course: int
+    "Курс"
 
 @dataclass
 class Faculty(JSONDeserializable):
-    id:         int
-    shortName:  str
-    fullName:   str
+    "Моделька для факультетов"
+    id: int
+    "id факультета"
+    shortName: str
+    "Аббревиатура факультета(Зависит от языка приложения)"
+    fullName: str
+    "Название факультета(Зависит от языка приложения)"
 
 @dataclass
 class Group(JSONDeserializable):
-    id:             int
-    name:           str
-    course:         int
-    priority:       int
-    educationForm:  int
+    "Моделька для групп"
+    id: int
+    "id группы"
+    name: str
+    "Название группы"
+    course: int
+    "Курс группы"
+    priority: int
+    "Приоритет вывода"
+    educationForm: int
+    "Форма обучения"
 
 @dataclass
 class Person(JSONDeserializable):
-    id:         int
-    firstName:  str
+    "Моделька для персон"
+    id: int
+    "id преподавателя"
+    firstName: str
+    "Имя преподавателя"
     secondName: str
-    lastName:   str
+    "Отчество преподавателя"
+    lastName: str
+    "Фамилия преподавателя"
 
 @dataclass
 class Rd(JSONDeserializable):
+    "Моделька для объявлений в расписании"
     html: str
+    "Объявление"
 
 @dataclass
 class Structure(JSONDeserializable):
-    id:         int
-    shortName:  str
-    fullName:   str
+    "Моделька для структур"
+    id: int
+    "id структуры"
+    shortName: str
+    "Аббревиатура структуры(Зависит от языка приложения)"
+    fullName: str
+    "Название структуры(Зависит от языка приложения)"
 
 @dataclass
 class Student(JSONDeserializable):
+    "Моделька для Студентов"
     id: int
-    firstName:  str
+    "id студента"
+    firstName: str
+    "Имя студента"
     secondName: str
-    lastName:   str
+    "Отчество студента"
+    lastName: str
+    "Фамилия студента"
 
 @dataclass
 class TeacherByName(JSONDeserializable):
-    chairName:  str
-    id:         int
-    firstName:  str
+    "Моделька для поиска преподавателя"
+    chairName: str
+    "Название кафедры"
+    id: int
+    "id преподавателя"
+    firstName: str
+    "Имя преподавателя"
     secondName: str
-    lastName:   str
+    "Отчество преподавателя"
+    lastName: str
+    "Фамилия преподавателя"
 
 @dataclass
 class TimeTablePeriod(JSONDeserializable):
-    r1:                     int
-    rz14:                   int
-    rz15:                   int
-    r5:                     int
-    disciplineId:           int
-    educationDisciplineId:  int
-    disciplineFullName:     str
-    disciplineShortName:    str
-    classroom:              str
-    timeStart:              str
-    timeEnd:                str
-    teachersName:           str
-    teachersNameFull:       str
-    chairName:              str
-    type:                   int
-    typeStr:                str # For some reason this field is missing in the documentation
-    dateUpdated:            str
-    nonstandardTime:        bool
-    groups:                 str
-    extraText:              bool
+    "Моделька для занятия в расписании"
+    r1: int
+    "Код занятия"
+    rz14: int # По шапке бы надавал за такие названия
+    rz15: int
+    r5: int
+    disciplineId: int
+    "Идентификатор глобальной дисциплины"
+    educationDisciplineId: int
+    "Идентификатор дисциплины учебного плана"
+    disciplineFullName: str
+    "Полное название дисциплины"
+    disciplineShortName: str
+    "Сокращенное название дисциплины"
+    classroom: str
+    "Название аудитории"
+    timeStart: str
+    "Время начала"
+    timeEnd: str
+    "Время конца"
+    teachersName: str
+    "Сокращенные имена преподавателей"
+    teachersNameFull: str
+    "ФИО преподавателей"
+    chairName: str # Missing in the docs
+    "Название кафедры"
+    type: int
+    "Тип занятия"
+    typeStr: str # Missing in the docs
+    "Тип занятия (строковый формат)"
+    dateUpdated: str
+    "Дата последнего обновления"
+    nonstandardTime: bool
+    "Нестандартное время занятия"
+    groups: str
+    "Названия групп"
+    extraText: bool
+    "Есть ли объявление"
 
 @dataclass
 class TimeTableLesson(JSONDeserializable):
-    number:         int
-    periods:        list[TimeTablePeriod]
+    "Моделька для пары в расписании"
+    number: int
+    "Номер занятия"
+    periods: list[TimeTablePeriod]
+    "Список занятий"
 
     @staticmethod
-    def from_json(raw: dict | List[dict]):
+    def from_json(raw: dict | List[dict]) -> 'TimeTableLesson' | List['TimeTableLesson']:
         if isinstance(raw, list):
             res = []
             for r in raw:
@@ -122,11 +191,14 @@ class TimeTableLesson(JSONDeserializable):
 
 @dataclass
 class TimeTableDate(JSONDeserializable):
-    date:       str
-    lessons:    list[TimeTableLesson]
+    "Моделька для дня в расписании"
+    date: str
+    "Дата"
+    lessons: list[TimeTableLesson]
+    "Список пар"
 
     @staticmethod
-    def from_json(raw: dict | List[dict]):
+    def from_json(raw: dict | List[dict]) -> 'TimeTableDate' | List['TimeTableDate']:
         if isinstance(raw, list):
             res = []
             for r in raw:
@@ -140,15 +212,24 @@ class TimeTableDate(JSONDeserializable):
 
 @dataclass
 class TeacherIdentifier(JSONDeserializable):
-    id:     int
-    hash:   str
+    "Моделька для преподавателей (связь с другими системами)"
+    id: int
+    "id преподавателя"
+    hash: str
+    "Hash"
 
 @dataclass
 class StudentIdentifier(JSONDeserializable):
-    id:     int
-    hash:   str
+    "Моделька для студента (связь с другими системами)"
+    id: int
+    "id обучения студента"
+    hash: str
+    "Hash"
 
 @dataclass
 class Version(JSONDeserializable):
+    "Моделька для версии"
     name: str
+    "Версия"
     code: str
+    "Код версии"
