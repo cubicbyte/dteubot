2a1b332096afd39f8e8bfb7448cade2cddbff2ee
Bohdan Marukhnenko <bmaruhnenko@gmail.com>
2023-03-29 16:24:42 +0300

    error_handler.py: added the old message deletion handler

--- bot/error_handler.py
+++ bot/error_handler.py
@@ -3,29 +3,35 @@ import traceback
 from datetime import datetime
 from telegram import Update, Bot
 from telegram.error import BadRequest, TelegramError, NetworkError, Forbidden
-from telegram.ext import ContextTypes
-from .data import ChatData
+from telegram.ext import ContextTypes, CallbackContext
+from .data import ChatData, UserData
 from .utils.smart_split import smart_split
 
 _logger = logging.getLogger(__name__)
 log_chat_id: int | str = None
 
 async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # For some reason this is not applied automatically until this point
+    context._user_data = UserData(update.effective_user.id)
+    context._chat_data = ChatData(update.effective_chat.id)
+
     if isinstance(context.error, BadRequest):
         _logger.warn(context.error)
         if context.error.message.startswith('Message is not modified'):
             return
         if context.error.message.startswith('Chat not found') or \
                 context.error.message.startswith('Peer_id_invalid'):
-            _chat_data = ChatData(update.effective_chat.id) # For some reason context._chat_data is None
-            _chat_data.set('_accessible', False)
+            context._chat_data.set('_accessible', False)
+            return
+        if context.error.message.startswith('Message can\'t be deleted for everyone'):
+            if isinstance(context, CallbackContext):
+                await update.callback_query.answer(text=context._chat_data.get_lang()['alert.message_too_old'])
             return
 
     if isinstance(context.error, Forbidden):
         # Bot was blocked by the user
         _logger.warn(context.error)
-        _chat_data = ChatData(update.effective_chat.id)
-        _chat_data.set('_accessible', False)
+        context._chat_data.set('_accessible', False)
         return
 
     if isinstance(context.error, NetworkError) and context.error.message.startswith('httpx'):
