93b5f0480f3af9e617093a985ff5631ee1f93d30
Bohdan Marukhnenko <bmaruhnenko@gmail.com>
2023-10-13 10:01:38 +0300

    Added tgbotapi error support for error handler

--- internal/dteubot/errorhandler/handler.go
+++ internal/dteubot/errorhandler/handler.go
@@ -10,6 +10,7 @@ import (
 	"github.com/cubicbyte/dteubot/pkg/api"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/op/go-logging"
+	"github.com/sirkon/go-format/v2"
 	"net/http"
 	"net/url"
 	"os"
@@ -20,6 +21,7 @@ var log = logging.MustGetLogger("ErrorHandler")
 func HandleError(err error, update tgbotapi.Update) {
 	var urlError *url.Error
 	var httpApiError *api.HTTPApiError
+	var tgError *tgbotapi.Error
 
 	switch {
 	case errors.As(err, &urlError):
@@ -125,6 +127,93 @@ func HandleError(err error, update tgbotapi.Update) {
 		if err != nil {
 			log.Errorf("Error sending HTTPApiError page: %s", err)
 			SendErrorToTelegram(err)
+		}
+
+	case errors.As(err, &tgError):
+		// Telegram API error
+		switch tgError.Code {
+		case 403:
+			// User blocked bot
+			chat := update.FromChat()
+			if chat == nil {
+				break
+			}
+
+			// Get chat data
+			cm := utils.GetChatDataManager(chat.ID)
+			chatData, err := cm.GetChatData()
+			if err != nil {
+				log.Errorf("Error getting chat data: %s", err)
+				SendErrorToTelegram(err)
+				break
+			}
+
+			// Set chat data "accessible" to false
+			chatData.Accessible = false
+			err = cm.UpdateChatData(chatData)
+			if err != nil {
+				log.Errorf("Error updating chat data: %s", err)
+				SendErrorToTelegram(err)
+				break
+			}
+		case 420:
+			// Flood control exceeded
+			log.Warningf("Flood control exceeded, retry after %d: %s", tgError.RetryAfter, err)
+
+			// Get chat where error occurred
+			chat := update.FromChat()
+			if chat == nil {
+				break
+			}
+
+			// Send alert to chat
+			if update.CallbackQuery == nil {
+				// Can't edit message
+				break
+			}
+
+			lang, err := utils.GetLang(utils.GetChatDataManager(chat.ID))
+			if err != nil {
+				log.Errorf("Error getting chat language: %s", err)
+				break
+			}
+
+			text := format.Formatp(lang.Alert.FloodControl, tgError.ResponseParameters.RetryAfter)
+			_, err = settings.Bot.Request(tgbotapi.NewCallbackWithAlert(update.CallbackQuery.ID, text))
+			if err != nil {
+				log.Errorf("Error sending flood control alert: %s", err)
+				break
+			}
+
+		default:
+			// Unknown Telegram API error
+			log.Errorf("Unknown Telegram API error: %s", err)
+			SendErrorToTelegram(err)
+
+			// Send error page to user
+			chat := update.FromChat()
+			if chat == nil {
+				break
+			}
+			cm := utils.GetChatDataManager(chat.ID)
+			page, err := pages.CreateErrorPage(cm)
+			if err != nil {
+				log.Errorf("Error creating error page: %s", err)
+				SendErrorToTelegram(err)
+				break
+			}
+
+			if update.CallbackQuery != nil {
+				_, err = settings.Bot.Send(buttons.EditMessageRequest(page, update.CallbackQuery))
+			} else {
+				_, err = settings.Bot.Send(page.CreateMessage(cm.ChatId))
+			}
+
+			if err != nil {
+				log.Errorf("Error sending error page: %s", err)
+				SendErrorToTelegram(err)
+				break
+			}
 		}
 
 	default:
