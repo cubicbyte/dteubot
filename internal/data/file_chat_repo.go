e77c34d6916039e48624aa46e45528465ea73d2d
Bohdan Marukhnenko <bmaruhnenko@gmail.com>
2023-10-25 17:37:32 +0300

    Added chat database repo implementation

---
+++ internal/data/file_chat_repo.go
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2022 Bohdan Marukhnenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package data
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"strconv"
+)
+
+// FileChatRepository implements ChatRepository interface using file system.
+//
+// Should be created via NewFileChatRepository.
+type FileChatRepository struct {
+	dir string
+}
+
+// NewFileChatRepository creates a new instance of FileChatRepository.
+func NewFileChatRepository(dir string) *FileChatRepository {
+	return &FileChatRepository{dir: dir}
+}
+
+func (r *FileChatRepository) GetById(id int64) (*Chat, error) {
+	chat := new(Chat)
+
+	file, err := os.Open(r.getChatFile(id))
+	if errors.Is(err, os.ErrNotExist) {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	err = json.NewDecoder(file).Decode(chat)
+	if err != nil {
+		return nil, err
+	}
+
+	return chat, nil
+}
+
+func (r *FileChatRepository) Update(chat *Chat) error {
+	file, err := os.OpenFile(r.getChatFile(chat.Id), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	err = json.NewEncoder(file).Encode(chat)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// getChatFile returns a path to a file with chat data.
+func (r *FileChatRepository) getChatFile(id int64) string {
+	return r.dir + "/" + strconv.FormatInt(id, 10) + ".json"
+}
---
+++ internal/data/file_user_repo.go
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2022 Bohdan Marukhnenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package data
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"strconv"
+)
+
+// FileUserRepository implements UserRepository interface by storing data in files.
+//
+// Should be created via NewFileUserRepository.
+type FileUserRepository struct {
+	dir string
+}
+
+// NewFileUserRepository creates a new instance of FileUserRepository.
+func NewFileUserRepository(dir string) *FileUserRepository {
+	return &FileUserRepository{dir: dir}
+}
+
+func (r *FileUserRepository) GetById(id int64) (*User, error) {
+	user := new(User)
+
+	file, err := os.Open(r.getUserFile(id))
+	if errors.Is(err, os.ErrNotExist) {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	err = json.NewDecoder(file).Decode(user)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (r *FileUserRepository) Update(user *User) error {
+	file, err := os.OpenFile(r.getUserFile(user.Id), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	err = json.NewEncoder(file).Encode(user)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// getUserFile returns a path to the file with user data.
+func (r *FileUserRepository) getUserFile(id int64) string {
+	return r.dir + "/" + strconv.FormatInt(id, 10) + ".json"
+}
